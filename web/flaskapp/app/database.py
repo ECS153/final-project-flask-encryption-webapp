7733a6ee0c2d08eb956bf1c584dd5b7a984c9e71
Noah Stern <noahastern@gmail.com>
2020-05-10 16:11:37 -0700

    Fixed firebase config, now have firebase admin set up in the database file.

--- web/flaskapp/app/database.py
+++ web/flaskapp/app/database.py
@@ -1,8 +1,9 @@
+import firebase_admin
 from firebase_admin import auth
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 
 class Database:
-  cred = credentials.RefreshToken('./app/config/serviceAccountKey.json')
+  cred = credentials.Certificate('./app/config/serviceAccountKey.json')
   default_app = firebase_admin.initialize_app(cred)
